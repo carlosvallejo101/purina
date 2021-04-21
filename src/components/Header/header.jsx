b035bf2eace1a20017d42135e4a2d5d8b405fa8b
Carlos Vallejo <cvallejo.ec@gmail.com>
2021-04-21 04:26:49 -0500

    Header with name

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { useAuth } from '../../auth/useAuth.jsx';
 import './header.css';
 
 import LogoProplan from '../../assets/img/logo_proplan.png';
 
 const Header = () => {
+  const { user } = useAuth();
   return (
     <div className="header">
       <img src={LogoProplan} alt="logo" />
+      {user && <h2>Hola! {user.name.split(' ')[0]}</h2>}
     </div>
   );
 };
