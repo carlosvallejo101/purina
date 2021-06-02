ac7b6577e7e814bdf6c8214cc7bd791e4ff6669a
Carlos Vallejo <cvallejo.ec@gmail.com>
2021-06-02 14:16:50 -0500

    Remove from cart

--- src/pages/Store/index.jsx
+++ src/pages/Store/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import axios from 'axios';
+import BeenhereIcon from '@material-ui/icons/Beenhere';
 
 import './store.css';
 import { backendSQL } from '../../config';
@@ -12,7 +12,7 @@ export const Store = () => {
   const [products, setProducts] = useState({});
   const [availablePoints, setAvailablePoints] = useState(null);
   const [remainingPoints, setRemainingPoints] = useState(null);
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState(null);
 
   useEffect(() => {
     if (user) {
@@ -31,7 +31,7 @@ export const Store = () => {
         .then(({ data: points }) => {
           setRemainingPoints(points);
           setAvailablePoints(points);
-          setCart(JSON.parse(localStorage.getItem('purinaCart')));
+          setCart(JSON.parse(localStorage.getItem('purinaCart') || []));
         })
         .catch((e) => {
           console.log(e);
@@ -40,8 +40,8 @@ export const Store = () => {
   }, [user]);
 
   useEffect(() => {
-    updateRemainingPoints();
-    if (cart.length > 0) {
+    if (cart) {
+      updateRemainingPoints();
       localStorage.setItem('purinaCart', JSON.stringify(cart));
     }
   }, [cart]);
@@ -58,10 +58,15 @@ export const Store = () => {
     ]);
   };
 
+  const removeFromCart = (product) => {
+    setCart(cart.filter((item) => item.id !== product.id));
+  };
+
   const updateRemainingPoints = () => {
-    cart.map((item) => {
-      setRemainingPoints(remainingPoints - item.cost);
-    });
+    const total = cart.reduce(function (prev, cur) {
+      return prev + cur.cost;
+    }, 0);
+    setRemainingPoints(availablePoints - total);
   };
 
   return (
@@ -71,7 +76,7 @@ export const Store = () => {
         isInStore: true,
         availablePoints,
         remainingPoints,
-        chosenAwards: cart.length,
+        chosenAwards: cart ? cart.length : 0,
       }}
     >
       <div className="store">
@@ -84,28 +89,48 @@ export const Store = () => {
               en el rango de puntos. <br /> Si finalizaste puedes regresar a
               revisar tu pedido para poder finalizarlo.
             </p>
-            {/* {cart.map((item) => (
-              <p className="text-light">{item.name}</p>
-            ))} */}
             <div className="store-product-items">
               {products.length > 0 &&
+                cart &&
                 products.map((product) => (
                   <div className="store-product" key={product.id}>
+                    {cart.find((item) => item.id === product.id) && (
+                      <BeenhereIcon
+                        style={{
+                          fill: '#96D93B',
+                          fontSize: '50px',
+                        }}
+                        className="hover-icon"
+                      />
+                    )}
                     <div className="store-product-content">
                       <div className="store-product-img">
                         <img src={product.imageName} alt={product.imageName} />
                       </div>
                       <div className="store-product-btns">
-                        <button
-                          type="button"
-                          className="btn-cart"
-                          onClick={() => addToCart(product)}
-                        >
-                          añadir al carrito
-                          <span>
-                            <i className="fas fa-plus" />
-                          </span>
-                        </button>
+                        {cart.find((item) => item.id === product.id) ? (
+                          <button
+                            type="button"
+                            className="btn-cart"
+                            onClick={() => removeFromCart(product)}
+                          >
+                            remover
+                            <span>
+                              <i className="fas fa-plus" />
+                            </span>
+                          </button>
+                        ) : (
+                          <button
+                            type="button"
+                            className="btn-cart"
+                            onClick={() => addToCart(product)}
+                          >
+                            añadir al carrito
+                            <span>
+                              <i className="fas fa-plus" />
+                            </span>
+                          </button>
+                        )}
                       </div>
                     </div>
                     <div className="store-product-info">
